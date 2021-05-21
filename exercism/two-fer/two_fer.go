7ac54a01793809efe139f59be9b98ac465336be5
James McClune <falcon78921@users.noreply.github.com>
2021-05-20 22:57:19 -0400

    improvements on two-fer

--- exercism/two-fer/two_fer.go
+++ exercism/two-fer/two_fer.go
@@ -1,11 +1,17 @@
+// Package twofer will take a name and output a certain string
 package twofer
 
+import "fmt"
+
+// ShareWith will take a name input (string) and return another string based on the name
 func ShareWith(name string) string {
     if name == "Alice" {
-        return "One for Alice, one for me."
+            return "One for Alice, one for me."
     } else if name == "Bob" {
-        return "One for Bob, one for me."
-    } else {
-        return "One for you, one for me."
+            return "One for Bob, one for me."
+    } else if name == "" {
+            return "One for you, one for me."
     }
+
+    return fmt.Sprintf("One for %s, one for me.", name)
 }
